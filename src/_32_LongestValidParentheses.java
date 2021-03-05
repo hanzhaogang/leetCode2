dc44e72d2ba45032107e8a59d914e458d2719c73
Hansen <hanzhaogang@foxmail.com>
2021-03-05 16:02:55 +0800

    32

--- src/_32_LongestValidParentheses.java
+++ src/_32_LongestValidParentheses.java
@@ -21,7 +21,7 @@ Output: 0
 
 Constraints:
 
-0 <= s.length <= 3 * 104
+0 <= s.length <= 3 * 10^4
 s[i] is '(', or ')'.
 
 来源：力扣（LeetCode）
@@ -31,8 +31,18 @@ s[i] is '(', or ')'.
 public class _32_LongestValidParentheses {
     
 }
+/*思路：
+0. 首先要注意审题，注意是substring，不是subsequence。找到所有的subsequence需要2^n指数级时间复杂度，而找到所有的substring只需要n^2
+1. BF， 找到所有的substring，针对每个substring判定其是否valid，进而找到最长的valid substring的长度。时间复杂度n^3。
+   因为数据规模3*10^4,所以时间复杂度应该在nlogn级别。 
+   因为字符串问题难以找到logn级别算法，所以推测应该有线性时间复杂的解法。
+   括号对问题一般可以通过stack解决。考虑是否可以通过一遍出栈入栈操作得到解。（？？？）
+
+   ()(()()()
+   首先出栈入栈一次，找到
+   */
 class Solution {
     public int longestValidParentheses(String s) {
-
+        
     }
 }
