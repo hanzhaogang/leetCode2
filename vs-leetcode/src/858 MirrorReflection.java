e8e5e2cf30aa5cdc4bc9d45f8c17814043b22bea
Hansen <hanzhaogang@foxmail.com>
2021-07-27 14:01:45 +0800

    0727office

---
+++ vs-leetcode/src/858 MirrorReflection.java
@@ -0,0 +1,44 @@
+public class 858 MirrorReflection {
+	
+}
+/* There is a special square room with mirrors on each of the four walls.  
+Except for the southwest corner, there are receptors on each of the remaining corners, numbered 0, 1, and 2.
+
+The square room has walls of length p, 
+and a laser ray from the southwest corner first meets the east wall at a distance q from the 0th receptor.
+
+Return the number of the receptor that the ray meets first.  
+(It is guaranteed that the ray will meet a receptor eventually.)
+
+ 
+
+Example 1:
+
+Input: p = 2, q = 1
+Output: 2
+Explanation: The ray meets receptor 2 the first time it gets reflected back to the left wall.
+
+Note:
+
+1 <= p <= 1000
+0 <= q <= p
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/mirror-reflection
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 
+
+
+思路： 
+数据规模可知复杂度为n^2
+一般这种应用题目需要抽象成它所对应的本质问题。
+
+---
+数学问题常常要举例子找出其规律。
+
+*/
+
+class Solution {
+    public int mirrorReflection(int p, int q) {
+
+    }
+}
