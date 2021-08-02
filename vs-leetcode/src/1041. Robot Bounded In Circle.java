183b599c468c8161ddc6f5832c454d17904ce7dd
Hansen <hanzhaogang@foxmail.com>
2021-08-02 15:56:00 +0800

    0802office

--- vs-leetcode/src/1041. Robot Bounded In Circle.java
+++ vs-leetcode/src/1041. Robot Bounded In Circle.java
@@ -53,6 +53,19 @@ instructions[i] is 'G', 'L' or, 'R'.
 模拟法中，针对每一步的指令，需要判定是不是已经存在重复路径。
 判定方法：首先确定所在位置之前有没有出现过；
 如果出现过，一次排查后面的每一步，
+
+标签： math string simulation
+提示： 
+Calculate the final vector of how the robot travels after executing all instructions once 
+- it consists of a change in position plus a change in direction.
+The robot stays in the circle iff (looking at the final vector) 
+it changes direction (ie. doesn't stay pointing north), or it moves 0.
+
+走完一遍指令之后，如果满足以下条件之一，则表示机器人会在环内行走。
+
+最后的位置在原始位置
+最后的方向跟初始方向不同
+
 */
 class Solution {
     public boolean isRobotBounded(String instructions) {
