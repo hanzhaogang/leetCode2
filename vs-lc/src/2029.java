5e1a8ab078fa3577e95181d0b83bc64cf424a8fd
Hansen <hanzhaogang@foxmail.com>
2022-01-20 14:51:26 +0800

    0120office

---
+++ vs-lc/src/2029.java
@@ -0,0 +1,18 @@
+public class 2029 {
+	    public boolean stoneGameIX(int[] stones) {//if alice the 1st player can win
+		Set<Integer> s0=new HashSet<>();
+		Set<Integer> s1=		
+    }
+    private boolean p1win(int sum,int c0,int c1,int c2){
+	    
+	if(sum==0){
+		if(c1==0&&c2==0&&c0!=0){
+			return false;
+		}
+		if(c1!=0||c2!=0){
+			return !p1win()
+		}
+	}
+
+    }
+}
