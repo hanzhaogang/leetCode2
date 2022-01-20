25cfc2f526d1578e0f852a759cd2de65d2562d03
Hansen <hanzhaogang@foxmail.com>
2022-01-20 21:57:07 +0800

    Update 2029.java

--- vs-lc/src/2029.java
+++ vs-lc/src/2029.java
@@ -3,16 +3,18 @@ public class 2029 {
 		Set<Integer> s0=new HashSet<>();
 		Set<Integer> s1=		
     }
-    private boolean p1win(int sum,int c0,int c1,int c2){
-	    
-	if(sum==0){
-		if(c1==0&&c2==0&&c0!=0){
-			return false;
-		}
-		if(c1!=0||c2!=0){
-			return !p1win()
-		}
+    private boolean p1win(int sum,int[] c){
+	    /* sum==0,c1 c2至少有一个!=0;
+	       sum==1,c0 c1至少一个；c0==0,new sum==2; c1==0,new sum==1;
+	       sum==2,c0,c2至少一个；c0==0,new sum==1; c2==0,new sum==2;
+	if(c[(sum+1)%3]==0&&c[(sum+2)%3]==0){
+		return false;
+	}else if(c[(sum+1)%3]==0){
+		return !p1win();
 	}
+
+
+
 
     }
 }
