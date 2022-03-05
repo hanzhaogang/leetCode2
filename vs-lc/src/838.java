be44fa1192a944349cfea994a5d4ba4d46319f5b
Hansen <hanzhaogang@foxmail.com>
2022-03-05 08:03:19 +0800

    0305

---
+++ vs-lc/src/838.java
@@ -0,0 +1,47 @@
+public class 838 {
+	
+}
+class Solution {
+    public String pushDominoes(String dominoes) {
+	int l=0,h=0;
+	StringBuilder sb=new StringBuilder();
+	while(h<dominoes.length()){
+		if(l==h&&dominoes.charAt(l)=='L'){
+			sb.append('L');
+			l++;
+			h++;
+		}else if(l==h&&dominoes.charAt(l)=='R'&&l==dominoes.length()-1){
+			sb.append('R');
+			l++;
+			h++;
+		}else if(l==h&&dominoes.charAt(l)=='R'&&dominoes.charAt(l+1)!='.'){
+			sb.append('R');
+			l++;
+			h++;
+		}else if(l==h&&dominoes.charAt(l)=='R'){
+			h++;
+		}else if(l==h&&dominoes.charAt(l)=='.'){
+			h++;
+		}else if(l<h&&dominoes.charAt(h)=='.'){
+			h++;
+		}else{
+			if(dominoes.charAt(h)=='L'){
+				if(dominoes.charAt(l)=='.'){
+					for(int i=0;i<h-l;i++){
+						sb.append(dominoes.charAt(l));
+					}
+					l=h;
+				}else{
+					
+				}
+			}else {
+				
+					for(int i=0;i<h-l;i++)
+						sb.append(dominoes.charAt(l));
+				
+				l=h;
+			}
+		}
+	}
+    }
+}
